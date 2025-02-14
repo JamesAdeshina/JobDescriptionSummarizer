47ff673aa0d798a09f4c70c81ea0724c184d241c
James <adeshina.james@bowen.edu.ng>
2025-02-14 18:58:45 +0000

    Implementing OOP...

---
+++ main_oop.py
@@ -0,0 +1,7 @@
+# Import required libraries
+import nltk
+import spacy
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer
+from collections import Counter
